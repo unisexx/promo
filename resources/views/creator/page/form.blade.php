c53827b2027c9eea1334abd71a0415038501d008
Dear <unisexx@gmail.com>
2017-11-09 15:40:03 +0700

    ลบไฟล์ที่ไม่ต้องการ

--- resources/views/creator/page/form.blade.php
+++ resources/views/creator/page/form.blade.php
@@ -66,47 +66,10 @@
   </div>
   <!-- /.content-wrapper -->
 
-  <!-- <script src = "{{ URL::to('js/tinymce/js/tinymce/tinymce.min.js') }}"></script>
-  <script>
-  var editor_config = {
-      path_absolute : "{{ URL::to('/') }}/",
-      selector: "textarea.tinymce",
-      plugins: [
-        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-        "searchreplace wordcount visualblocks visualchars code fullscreen",
-        "insertdatetime media nonbreaking save table contextmenu directionality",
-        "emoticons template paste textcolor colorpicker textpattern"
-      ],
-      toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
-      relative_urls: false,
-      file_browser_callback : function(field_name, url, type, win) {
-        var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-        var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
-
-        var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
-        if (type == 'image') {
-          cmsURL = cmsURL + "&type=Images";
-        } else {
-          cmsURL = cmsURL + "&type=Files";
-        }
-
-        tinyMCE.activeEditor.windowManager.open({
-          file : cmsURL,
-          title : 'Filemanager',
-          width : x * 0.8,
-          height : y * 0.8,
-          resizable : "yes",
-          close_previous : "no"
-        });
-      }
-    };
-
-    tinymce.init(editor_config);
-  </script> -->
-
   <script src = "{{ URL::to('js/tinymce_file_manager/tinymce/tinymce.min.js') }}"></script>
 <script>
 tinymce.init({
+
    selector: "textarea.tinymce",theme: "modern",
 	 height: 400,
    plugins: [
