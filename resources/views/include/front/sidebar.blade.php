4ee60924b3aa7976824c27f584e160f6786b6da2
unisexx <unisexx@gmail.com>
2017-11-11 03:22:02 +0700

    ...

--- resources/views/include/front/sidebar.blade.php
+++ resources/views/include/front/sidebar.blade.php
@@ -1,4 +1,8 @@
 <div class="list-group">
-	<a href="{{ url('sticker') }}" class="list-group-item">สติ๊กเกอร์ไลน์</a>
-	<a href="{{ url('theme') }}" class="list-group-item">ธีมไลน์</a>
+	<a href="{{ url('sticker') }}" class="list-group-item"><b>- สติ๊กเกอร์ไลน์</b></a>
+	<a href="{{ url('theme') }}" class="list-group-item"><b>- ธีมไลน์</b></a>
 </div>
+
+<style>
+	.list-group-item{border:none !important;}
+</style>
