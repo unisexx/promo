e06a29a19ce618bc0af5d4830cc9b014ab02f5a1
unisexx <unisexx@gmail.com>
2017-12-17 00:20:11 +0700

    ...

--- resources/views/widgets/social_share_btn.blade.php
+++ resources/views/widgets/social_share_btn.blade.php
@@ -8,4 +8,10 @@
     <a class="btn btn-social-icon btn-xs btn-google" href="https://plus.google.com/share?url={{ urldecode($url) }}" target="_blank">
         <i class="fa fa-google-plus"></i>
     </a>
+    <a href="http://line.me/R/msg/text/?{{ urldecode($url) }}">
+        <img src="image/share-b.png" alt="LINE it!" width="22" height="22" style="background-color:#00B833;border-radius:3px;border:1px solid rgba(0,0,0,0.2);">
+    </a>
+    <a class="btn btn-social-icon btn-xs btn-reddit" href="https://line.me/S/sticker/1472405" target="_blank">
+        <i class="fa fa-external-link" aria-hidden="true"></i>
+    </a>
 </div>
